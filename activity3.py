44ad52ec9851274fbd4b53e9a77659722b8aeb70
maheen063 <maheenshoaib063@gmail.com>
2026-02-07 12:53:54 +0500

    completed activity 3

---
+++ activity3.py
@@ -0,0 +1,8 @@
+def numberofBits(n):
+    count = 0
+    while (n):
+        count += 1
+        n >>= 1
+    return count
+number = int(input("Enter your number: "))
+print("Total bits: ", numberofBits(number))
